c11da60fbf4bb73b7892842a831ce95a4ff4237a
adamhinckley <adamhinckley@mac.com>
2018-11-30 09:41:37 -0700

    api/register works

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,9 +1,16 @@
 <!-- Answers to the Short Answer Essay Questions go here -->
 
 1. What is the purpose of using _sessions_?
+   `to store data about the user for authentication and other purposes`
 
-2. What does bcrypt do to help us store passwords in a secure manner.
+2) What does bcrypt do to help us store passwords in a secure manner.
+
+`it only stores the hashed password on the server so the actual password the user made can't be accessed`
 
 3. What does bcrypt do to slow down attackers?
 
+`it adds salt before hashing so the password can't be un-hashed`
+
 4. What are the three parts of the JSON Web Token?
+
+`header, payload, signature`
--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,28 @@
-const axios = require('axios');
+const axios = require("axios");
+const bcrypt = require("bcryptjs");
+const db = require("../database/dbConfig");
 
-const { authenticate } = require('./middlewares');
+const { authenticate } = require("./middlewares");
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
 function register(req, res) {
-  // implement user registration
+  //get username and password
+  creds = req.body;
+  //generate hash
+  const hash = bcrypt.hashSync(creds.password, 10);
+  //override pass with hash
+  creds.password = hash;
+  db("users")
+    .insert(creds)
+    .then(ids => {
+      res.status(201).json(ids);
+    })
+    .catch(err => json({ message: "Error adding user to the database", err }));
 }
 
 function login(req, res) {
@@ -18,13 +31,11 @@ function login(req, res) {
 
 function getJokes(req, res) {
   axios
-    .get(
-      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten'
-    )
+    .get("https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten")
     .then(response => {
       res.status(200).json(response.data);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
